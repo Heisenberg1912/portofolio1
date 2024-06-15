26c0db33eec5f02f42f6a7410faff28d81910c97
Heisenberg1912 <109282897+Heisenberg1912@users.noreply.github.com>
2024-06-15 13:10:14 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript/AI/ML these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Heisenberg1912" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and Listen to spotify. 
+  I also play games games Like Valorant. Go check <a href="https://open.spotify.com/user/x8ygah41clsvn57a9vngnq4ml" target="_blank">my profile</a>! (If you like the content)
+  check out the playlist i am curently playing`,
+  tv: `That's my TV. I've been scrolling instagram alot recently  like :
+   <a href="https://www.instagram.com/_tusshhaarr_/" target="_blank">_tusshhaarr_</a>, <a href="https://tracker.gg/valorant/profile/riot/WTFtusshhaarr%23lavde/overview" target="_blank">tracker.gg</a>,
+  <a href="https://www.youtube.com/channel/UCRHmNCJaso8PQbLZfRgD-wg" target="_blank">Personal Channel</a> (sometimes) `,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Heisenberg1912/Tushar-s-Resume/blob/main/tushar%20real%20resume.pdf" target="_blank">Check it out?</a>
+  Contact me at <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blank">batham.tushar2001@gmail.com</a>  if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : to showcase my potential with all the tech that i have used here , i want to make new connection and participate in task that will help me develop my skills even further, check my profile <a href="https://www.linkedin.com/in/tushar-batham-7792bb251/" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. I play guitar a lot with weird ass chord progress and sing terribly.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Tushar's portfolio, just close the tab.`,
+};
